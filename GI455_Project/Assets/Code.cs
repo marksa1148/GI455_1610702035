7ce7d0d85c8c3a255e3a4e3bace0b3833127c374
unknown <marksa1144@gmail.com>
2021-01-28 15:41:56 +0700

    Send Assignment Week1

---
+++ GI455_Project/Assets/Code.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Code : MonoBehaviour
+{
+
+    [SerializeField] string[] animeName;
+   
+ 
+
+    public void ButtonControls()
+    {
+        GameObject Search = GameObject.Find("SearchField");
+        InputField animeNameSearch = Search.GetComponent<InputField>();
+        string PlayerSearch = animeNameSearch.text;
+
+        GameObject found = GameObject.Find("Check");
+       
+
+
+        for (int i = 0;i < animeName.Length;i++)
+        {
+            if (animeName[i] == PlayerSearch)
+            {
+                found.GetComponent<Text>().text = PlayerSearch + " Found";
+                found.GetComponent<Text>().color = Color.green;
+                break;
+            }
+            
+            else
+            {
+                found.GetComponent<Text>().text = PlayerSearch +" not Found";
+                found.GetComponent<Text>().color = Color.red;
+            }
+
+        }
+    }
+
+}
